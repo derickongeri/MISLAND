c66535568f52ccc6ab660b7a37ff14876f445ba6
Grace Amondi <ecofluff990@gmail.com>
2021-02-16 22:59:08 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -69,7 +69,7 @@ html_theme_options = {
     #'vcs_pageview_mode': '',
     'style_nav_header_background': 'black',
     # Toc options
-    'collapse_navigation': True,
+    'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': False,
